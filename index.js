d0905612813b6cd6dbdad3ed748e1bbece03b48a
Ronaldo Moco <ronaldo.moco@telefonica.com>
2023-05-10 11:54:48 -0300

    remove unused code

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-const { TwilioWhatsAppAdapter } = require('@botbuildercommunity/adapter-twilio-whatsapp');
-
+const {
+    TwilioWhatsAppAdapter
+} = require('@botbuildercommunity/adapter-twilio-whatsapp');
 const path = require('path');
-
+const restify = require('restify');
 const dotenv = require('dotenv');
-// Import required bot configuration.
+
 const ENV_FILE = path.join(__dirname, '.env');
 dotenv.config({ path: ENV_FILE });
-
-const restify = require('restify');
-
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const {
-    CloudAdapter,
-    ConfigurationServiceClientCredentialFactory,
-    createBotFrameworkAuthenticationFromConfiguration
-} = require('botbuilder');
 
 // This bot's main dialog.
 const { EchoBot } = require('./bot');
@@ -35,22 +24,11 @@ server.use(restify.plugins.bodyParser());
 
 server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log(`\n${server.name} listening to ${server.url}`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log(
+        '\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator'
+    );
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
-
-const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
-    MicrosoftAppId: process.env.MicrosoftAppId,
-    MicrosoftAppPassword: process.env.MicrosoftAppPassword,
-    MicrosoftAppType: process.env.MicrosoftAppType,
-    MicrosoftAppTenantId: process.env.MicrosoftAppTenantId
-});
-
-const botFrameworkAuthentication = createBotFrameworkAuthenticationFromConfiguration(null, credentialsFactory);
-
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about adapters.
-const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 // Catch-all for errors.
 const onTurnErrorHandler = async (context, error) => {
@@ -69,11 +47,12 @@ const onTurnErrorHandler = async (context, error) => {
 
     // Send a message to the user
     await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+    await context.sendActivity(
+        'To continue to run this bot, please fix the bot source code.'
+    );
 };
 
-// Set the onTurnError for the singleton CloudAdapter.
-adapter.onTurnError = onTurnErrorHandler;
+whatsAppAdapter.onTurnError = onTurnErrorHandler;
 
 // Create the main dialog.
 const myBot = new EchoBot();
@@ -84,15 +63,4 @@ server.post('/api/whatsapp/messages', async (req, res) => {
         // Route to main dialog.
         await myBot.run(context);
     });
-});
-
-// Listen for Upgrade requests for Streaming.
-server.on('upgrade', async (req, socket, head) => {
-    // Create an adapter scoped to this WebSocket connection to allow storing session data.
-    const streamingAdapter = new CloudAdapter(botFrameworkAuthentication);
-
-    // Set onTurnError for the CloudAdapter created for each connection.
-    streamingAdapter.onTurnError = onTurnErrorHandler;
-
-    await streamingAdapter.process(req, socket, head, (context) => myBot.run(context));
 });
